a541fb10c98107c11e17353c3eaf139c61de8584
Bechir Lahoueg <bechir.lahoueg2019@gmail.com>
2026-04-03 22:04:44 +0100

    chore: remove JAVA directory

--- JAVA/Gestionformation/src/main/java/org/example/main/AppLauncher.java
+++
@@ -1,8 +0,0 @@
-package org.example.main;
-
-public class AppLauncher {
-    public static void main(String[] args) {
-        // Cette ligne appelle ton vrai Main JavaFX
-        MainFX.main(args);
-    }
-}
